173e6778c4712edc7c859fa7f61ea985f7c0a409
mariebawanan <bawananmarieflor@gmail.com>
2019-03-04 00:44:12 +0800

    Added authentication

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './signin.css';
 import FormField from '../widgets/formfields/formfields';
+import { firebase } from '../../firebase';
 
 class SignIn extends Component {
 
@@ -94,21 +95,94 @@ class SignIn extends Component {
         return error;
     }
 
+    submitForm = (event, type) => {
+        event.preventDefault();
+        if(type !== null) {
+            let dataToSubmit = {};
+            let formIsValid = true;
+
+            for (let key in this.state.formData) {
+                dataToSubmit[key] = this.state.formData[key].value
+            }
+
+            for(let key in this.state.formData){
+                formIsValid = this.state.formData[key].valid && formIsValid;
+            }
+
+            if(formIsValid) {
+                this.setState({
+                    loading: true,
+                    registerError: ''
+                })
+                if(type) {
+                    firebase.auth()
+                    .signInWithEmailAndPassword(
+                        dataToSubmit.email,
+                        dataToSubmit.password
+                    ).then(()=>{
+                        this.props.history.push('/')
+                    }).catch( error => {
+                        this.setState({
+                            loading: false,
+                            registerError: error.message
+                        })
+                    });
+                }
+                else {
+                    firebase.auth()
+                    .createUserWithEmailAndPassword(
+                        dataToSubmit.email,
+                        dataToSubmit.password
+                    ).then(()=>{
+                        this.props.history.push('/')
+                    }).catch( error => {
+                        this.setState({
+                            loading: false,
+                            registerError: error.message
+                        })
+                    });
+                }
+            }
+        }
+    }
+
+    submitButton = () => (
+        this.state.loading ? 
+            'loading...' :
+            <div>
+                <button onClick={ (event) => this.submitForm(event, false)}>Register now</button>
+                <button onClick={ (event) => this.submitForm(event, true)}>Log in</button>
+                </div>
+    )
+
+    showError = () => (
+        this.state.registerError !== '' ?
+            <div className={styles.error}>{this.state.registerError}</div>
+            : ''
+    )
+
+
     render(){
         return(
             <div className={styles.logContainer}>
-            <h2>Register / Log in </h2>
-                <FormField
-                    id={'email'}
-                    formData={this.state.formData.email}
-                    change={(element)=>this.updateForm(element)}
-                />
+                <form onSubmit={ (event) => this.submitForm(event, null)}>
+                    <h2>Register / Log in </h2>
+                        <FormField
+                            id={'email'}
+                            formData={this.state.formData.email}
+                            change={(element)=>this.updateForm(element)}
+                        />
 
-                <FormField
-                    id={'password'}
-                    formData={this.state.formData.password}
-                    change={(element)=>this.updateForm(element)}
-                />
+                        <FormField
+                            id={'password'}
+                            formData={this.state.formData.password}
+                            change={(element)=>this.updateForm(element)}
+                        />
+
+                        { this.submitButton() }
+                        { this.showError() }
+                    
+                </form>
             </div>
 
         )
