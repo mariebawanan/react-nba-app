9da2baeb628eb8271d193e852b70355e3ba27c2e
mariebawanan <bawananmarieflor@gmail.com>
2019-03-21 23:39:29 +0800

    Code clean up, used Prettier

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,275 +1,261 @@
-import React, { Component } from 'react';
-import styles from './dashboard.css';
-import { firebase,  firebaseTeams, firebaseArticles } from '../../firebase';
+import React, { Component } from "react";
+import styles from "./dashboard.css";
+import { firebase, firebaseTeams, firebaseArticles } from "../../firebase";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import { stateToHTML } from "draft-js-export-html";
 
-import FormField from '../widgets/formfields/formfields';
-
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
-import {stateToHTML} from 'draft-js-export-html';
-import Uploader from '../widgets/FileUploader/fileUploader';
+import FormField from "../widgets/formfields/formfields";
+import Uploader from "../widgets/FileUploader/fileUploader";
 
 class Dashboard extends Component {
-    
-    state = {
-        editorState: EditorState.createEmpty(),
-        postError:'',
-        loading: false,
-        formData: {
-            author: {
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'author_input',
-                    type: 'text',
-                    placeholder: 'Enter your name',
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                validationMessage:''
-            },
-            title: {
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'title_input',
-                    type: 'text',
-                    placeholder: 'Enter the title',
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                validationMessage:''
-            },
-            body: {
-                element: 'texteditor',
-                value:'',
-                valid: true
-            },
-            team: {
-                element: 'select',
-                value: '',
-                config: {
-                    name: 'team_input',
-                    options: []
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false,
-                validationMessage:''
-            },
-            image: {
-                element: 'image',
-                value:'',
-                valid: true
-            }
-        }
+  state = {
+    editorState: EditorState.createEmpty(),
+    postError: "",
+    loading: false,
+    formData: {
+      author: {
+        element: "input",
+        value: "",
+        config: {
+          name: "author_input",
+          type: "text",
+          placeholder: "Enter your name"
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      title: {
+        element: "input",
+        value: "",
+        config: {
+          name: "title_input",
+          type: "text",
+          placeholder: "Enter the title"
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      body: {
+        element: "texteditor",
+        value: "",
+        valid: true
+      },
+      team: {
+        element: "select",
+        value: "",
+        config: {
+          name: "team_input",
+          options: []
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      image: {
+        element: "image",
+        value: "",
+        valid: true
+      }
+    }
+  };
+
+  componentDidMount() {
+    this.loadteam();
+  }
+
+  loadteam = () => {
+    firebaseTeams.once("value").then(snapshot => {
+      let team = [];
+      snapshot.forEach(childSnapshot => {
+        team.push({
+          id: childSnapshot.val().teamId,
+          name: childSnapshot.val().city
+        });
+      });
+
+      const newFormData = { ...this.state.formData };
+      const newElement = { ...newFormData["team"] };
+
+      newElement.config.options = team;
+
+      newFormData["team"] = newElement;
+
+      this.setState({
+        formData: newFormData
+      });
+    });
+  };
+
+  updateForm = (element, content = "") => {
+    const newFormData = {
+      ...this.state.formData
+    };
+
+    const newElement = {
+      ...newFormData[element.id]
+    };
+
+    if (content === "") {
+      newElement.value = element.event.target.value;
+    } else {
+      newElement.value = content;
     }
 
-    componentDidMount(){
-        this.loadteam()
+    if (element.blur) {
+      let validData = this.validate(newElement);
+      newElement.valid = validData[0];
+      newElement.validationMessage = validData[1];
+    }
+    newElement.touched = element.blur;
+    newFormData[element.id] = newElement;
+
+    this.setState({
+      formData: newFormData
+    });
+  };
+
+  validate = element => {
+    let error = [true, ""];
+    if (element.validation.required) {
+      const valid = element.value.trim() !== "";
+      const message = `${!valid ? "This field is required! " : ""}`;
+      error = !valid ? [valid, message] : error;
+    }
+    return error;
+  };
+
+  submitForm = event => {
+    event.preventDefault();
+
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    for (let key in this.state.formData) {
+      dataToSubmit[key] = this.state.formData[key].value;
     }
 
-    loadteam = () => {
-        firebaseTeams.once('value')
-        .then((snapshot) => {
-            let team =  [];
-            snapshot.forEach((childSnapshot) => {
-                team.push({
-                    id: childSnapshot.val().teamId,
-                    name: childSnapshot.val().city
-                })
+    console.log(dataToSubmit);
+    console.log(dataToSubmit);
+    for (let key in this.state.formData) {
+      formIsValid = this.state.formData[key].valid && formIsValid;
+    }
+
+    if (formIsValid) {
+      this.setState({
+        loading: true,
+        postError: ""
+      });
+
+      firebaseArticles
+        .orderByChild("id")
+        .limitToLast(1)
+        .once("value")
+        .then(snapshot => {
+          let articleId = null;
+          snapshot.forEach(childSnapshot => {
+            articleId = childSnapshot.val().id;
+          });
+
+          dataToSubmit["date"] = firebase.database.ServerValue.TIMESTAMP;
+          dataToSubmit["id"] = articleId + 1;
+          dataToSubmit["team"] = parseInt(dataToSubmit["team"]);
+
+          firebaseArticles
+            .push(dataToSubmit)
+            .then(article => {
+              this.props.history.push(`/articles/${article.key}`);
             })
-
-            const newFormData = { ...this.state.formData}
-            const newElement = {...newFormData['team']}
-
-            newElement.config.options = team;
-
-            newFormData['team'] = newElement;
-
-            this.setState({
-                formData: newFormData
-            })
-        })
-        
+            .catch(e => {
+              this.setState({
+                postError: e.message
+              });
+            });
+        });
+    } else {
+      this.setState({
+        postError: "Something went wrong"
+      });
     }
+  };
 
-    updateForm = (element, content = '') => {
-        const newFormData = {
-            ...this.state.formData
-        }
+  submitButton = event =>
+    this.state.loading ? (
+      "loading..."
+    ) : (
+      <div>
+        <button type="submit"> Add Post </button>
+      </div>
+    );
 
-        const newElement = {
-            ...newFormData[element.id]
-        }
-        console.log(element);
-        if(content===''){
-            newElement.value = element.event.target.value;
-        }else{
-            newElement.value = content;
-        }
-        
-        if(element.blur){
-            let validData = this.validate(newElement);
-            newElement.valid = validData[0];
-            newElement.validationMessage = validData[1];
-        }
-        newElement.touched = element.blur;
-        newFormData[element.id] = newElement;
+  showError = () =>
+    this.state.postError !== "" ? (
+      <div className={styles.error}>{this.state.postError}</div>
+    ) : (
+      ""
+    );
 
-        this.setState({
-            formData: newFormData
-        })
-    }
+  onEditorStateChange = editorState => {
+    let contentState = editorState.getCurrentContent();
+    let html = stateToHTML(contentState);
+    this.updateForm({ id: "body" }, html);
+    this.setState({
+      editorState
+    });
+  };
 
+  storeFileName = filename => {
+    this.updateForm({ id: "image" }, filename);
+  };
 
-    validate = (element) => {
-        let error = [true, ''];
-        if(element.validation.required){
-            const valid = element.value.trim() !== '';
-            const message = `${!valid ? 'This field is required! ' : ''}`;
-            error = !valid ? [valid, message] : error;        
-        }
-
-        return error;
-    }
-
-    submitForm = (event) => {
-        event.preventDefault();
-
-        let dataToSubmit = {};
-        let formIsValid = true;
-
-        for (let key in this.state.formData) {
-            dataToSubmit[key] = this.state.formData[key].value
-        }
-
-        console.log(dataToSubmit);
-        console.log(dataToSubmit);
-        for(let key in this.state.formData){
-            formIsValid = this.state.formData[key].valid && formIsValid;
-        }
-
-        if(formIsValid) {
-            this.setState({
-                loading: true,
-                postError:''
-            })
-
-            firebaseArticles.orderByChild("id")
-            .limitToLast(1).once('value')
-            .then( snapshot => {
-                let articleId = null;
-                snapshot.forEach(childSnapshot => {
-                    articleId = childSnapshot.val().id;
-                })
-                dataToSubmit['date'] = firebase.database.ServerValue.TIMESTAMP;
-                dataToSubmit['id'] = articleId + 1;
-                dataToSubmit['team'] = parseInt(dataToSubmit['team'])
-
-                firebaseArticles.push(dataToSubmit)
-                .then( article => {
-                    this.props.history.push(`/articles/${article.key}`)
-                }).catch( e => {
-                    this.setState({
-                        postError: e.message
-                    })
-                })
-            })
-        }
-        else {
-            this.setState({
-                postError: 'Something went wrong'
-            })
-        }
-
-    }
-
-    submitButton = (event) => (
-        this.state.loading ? 
-            'loading...' :
-            <div>
-                <button type="submit" > Add Post </button>
-            </div>
-    )
-
-    showError = () => (
-        this.state.postError !== '' ?
-            <div className={styles.error}>{this.state.postError}</div>
-            : ''
-    )
-
-    onEditorStateChange = (editorState) => {
-        
-       
-        let contentState = editorState.getCurrentContent();
-
-        let html = stateToHTML(contentState);
-
-        this.updateForm({id:'body'}, html)
-
-        this.setState({
-            editorState
-        })
-    }
-
-    storeFileName = (filename) => {
-        this.updateForm({id:'image'}, filename)
-    }
-
-    render() {
-        return (
-
-            <div className={styles.postContainer}>
-                <form onSubmit={this.submitForm}>
-                    <h2> Add post </h2>
-
-                    <Uploader 
-                        filename={(filename)=>{this.storeFileName(filename)}}
-                    />
-
-                    <FormField
-                            id={'author'}
-                            formData={this.state.formData.author}
-                            change={(element)=>this.updateForm(element)}
-                        />
-                    
-                    <FormField
-                            id={'title'}
-                            formData={this.state.formData.title}
-                            change={(element)=>this.updateForm(element)}
-                        />
-
-                    <Editor 
-                        editorState = {this.state.editorState}
-                        wrapperClassName="myEditor-wrapper"
-                        editorClassName="myEditor-editor"
-                        onEditorStateChange={this.onEditorStateChange}
-                    />
-
-                    <FormField
-                            id={'team'}
-                            formData={this.state.formData.team}
-                            change={(element)=>this.updateForm(element)}
-                        />
-                    
-
-
-                    { this.submitButton() }
-                    { this.showError() }
-                </form>
-            </div>  
-        )
-    }
+  render() {
+    return (
+      <div className={styles.postContainer}>
+        <form onSubmit={this.submitForm}>
+          <h2> Add post </h2>
+          <Uploader
+            filename={filename => {
+              this.storeFileName(filename);
+            }}
+          />
+          <FormField
+            id={"author"}
+            formData={this.state.formData.author}
+            change={element => this.updateForm(element)}
+          />
+          <FormField
+            id={"title"}
+            formData={this.state.formData.title}
+            change={element => this.updateForm(element)}
+          />
+          <Editor
+            editorState={this.state.editorState}
+            wrapperClassName="myEditor-wrapper"
+            editorClassName="myEditor-editor"
+            onEditorStateChange={this.onEditorStateChange}
+          />
+          <FormField
+            id={"team"}
+            formData={this.state.formData.team}
+            change={element => this.updateForm(element)}
+          />
+          {this.submitButton()}
+          {this.showError()}
+        </form>
+      </div>
+    );
+  }
 }
-
 
 export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,73 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import FormField from './components/widgets/formfields/formfields';
+import React from "react";
+import { Switch } from "react-router-dom";
 
-import Home from './components/Home/home';
-import Layout from './hoc/Layout/layout';
+import Home from "./components/Home/home";
+import Layout from "./hoc/Layout/layout";
 
-import NewsArticle from './components/Articles/News/Post/index';
-import VideoArticle from './components/Articles/Videos/Video/index'
-import NewsMain from './components/Articles/News/Main/index';
-import VideosMain from './components/Articles/Videos/Main/index';
-import SignIn from './components/signin/signin';
-import Dashboard from './components/Dashboard/dashboard';
+import NewsArticle from "./components/Articles/News/Post/index";
+import VideoArticle from "./components/Articles/Videos/Video/index";
+import NewsMain from "./components/Articles/News/Main/index";
+import VideosMain from "./components/Articles/Videos/Main/index";
+import SignIn from "./components/signin/signin";
+import Dashboard from "./components/Dashboard/dashboard";
+import PrivateRoutes from "./components/AuthRoutes/privateRoutes";
+import PublicRoutes from "./components/AuthRoutes/publicRoutes";
 
-const Routes = (props) => {
-    return(
-        <Layout user = {props.user}>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/news" exact component={NewsMain}/>
-                <Route path="/articles/:id" exact component={NewsArticle}/>
-                <Route path="/videos/:id" exact component={VideoArticle}/>
-                <Route path="/videos" exact component={VideosMain}/>
-                <Route path="/sign-in" exact component={SignIn}/>
-                <Route path="/dashboard" exact component={Dashboard}/>
-            </Switch>
-        </Layout>
-        
-    )
-}
+const Routes = props => {
+  return (
+    <Layout user={props.user}>
+      <Switch>
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/"
+          exact
+          component={Home}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/news"
+          exact
+          component={NewsMain}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/articles/:id"
+          exact
+          component={NewsArticle}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/videos/:id"
+          exact
+          component={VideoArticle}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/videos"
+          exact
+          component={VideosMain}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={true}
+          path="/sign-in"
+          exact
+          component={SignIn}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+      </Switch>
+    </Layout>
+  );
+};
 
 export default Routes;
